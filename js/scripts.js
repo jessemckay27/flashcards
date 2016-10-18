9d2e48e7f5e482d75e3a73b368658a8be02f2468
Jesse McKay and Justin Xu <justin@>
2016-10-18 13:07:41 -0700

    add a list of clickable paragrahps under their definitions

---
+++ js/scripts.js
@@ -0,0 +1,113 @@
+$(document).ready(function() {
+  $("#one").click(function() {
+    $("#one-p").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#two").click(function() {
+    $("#two-p").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#three").click(function() {
+    $("#three-p").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#four").click(function() {
+    $("#four-p").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#five").click(function() {
+    $("#five-p").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#six").click(function() {
+    $("#six-p").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#seven").click(function() {
+    $("#seven-p").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#eight").click(function() {
+    $("#eight-p").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#nine").click(function() {
+    $("#nine-p").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#ten").click(function() {
+    $("#ten-p").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#eleven").click(function() {
+    $("#eleven-p").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#twelve").click(function() {
+    $("#twelve-p").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#thirteen").click(function() {
+    $("#thirteen-p").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#fourteen").click(function() {
+    $("#fourteen-p").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#fifteen").click(function() {
+    $("#fifteen-p").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#sixteen").click(function() {
+    $("#sixteen-p").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#seventeen").click(function() {
+    $("#seventeen-p").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#eightteen").click(function() {
+    $("#eighteen-p").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#nineteen").click(function() {
+    $("#nineteen-p").toggle();
+  });
+});
